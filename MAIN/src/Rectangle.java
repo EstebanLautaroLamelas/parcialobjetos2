080711417b92da15d187e8786d157a3a6c2c12fa
MC2901 <mcerini02@gmail.com>
2022-11-24 14:57:09 -0300

    refactor: checked classes and fixed

--- MAIN/src/Rectangle.java
+++ MAIN/src/Rectangle.java
@@ -1,6 +1,26 @@
-public class Rectangle {
+public class Rectangle extends Shape {
  public double width;
  public double lenght;
 
- 
+ Rectangle(double width, double lenght, String color, boolean filled){
+  super(color, filled);
+  this.width = width;
+  this.lenght = lenght;
+ }
+ public double getLenght() {
+  return lenght;
+ }
+
+ public double getWidth() {
+  return width;
+ }
+
+ public void setLenght(double lenght) {
+  this.lenght = lenght;
+ }
+
+ public void setWidth(double width) {
+  this.width = width;
+ }
+
 }
--- MAIN/src/Square.java
+++ MAIN/src/Square.java
@@ -1,2 +1,8 @@
 public class Square extends Rectangle{
+
+    Square(double width, double lenght, String color, boolean filled) {
+        super(width, lenght, color, filled);
+    }
+
+
 }
